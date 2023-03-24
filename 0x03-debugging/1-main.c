ec5e2c3cffd4db386869ca0a932269c280e1d80b
RGhassen <rghassen@outlook.fr>
2023-03-24 01:58:03 +0100

    commit generated by  shell script

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,11 +1,24 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 #include <stdio.h>
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
-#endif /* MAIN_H */
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
 
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/*
+	*while (i < 10)
+	*{
+	*putchar(i);
+	*}
+	*/
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
